f5411c024fc8ac1b64b791011b516da8df1fcee2
Predator5959 <predator595959@gmail.com>
2023-10-17 17:10:23 +0300

    Initial commit

--- Caltulator 2.0.py
+++ Caltulator 2.0.py
@@ -1,44 +1,62 @@
+error_number_only = 'please type only number'
+
 while True:
     while True:
         try:
             number_1 = float(input('Please type the number'))
-            if number_1 == int:
-                print('please type only number')
+            #$if number_1 == int:
+                #print('please type only number')
             break
         except ValueError:
-            print('please type only number')
+            print(error_number_only)
 
 
     while True:
+        zero_error = False
         try:
             number_2 = float(input('Please type the second number'))
-            if number_2 == int:
-                print('please type only number')
-            break
-        except ValueError:
-            print('please type only number')
-
-
-    while True:
-        try:
-            oper = input('Please type operation : + - / *')
-
-            if oper == '+':
-                print(number_1 + number_2)
-
-            elif oper == '-':
-                print(number_1 - number_2)
-
-            elif oper == '*':
-                print(number_1 * number_2)
-
-            elif oper == '/':
-                print(number_1 / number_2)
-
-            break
+            # if number_2 == int:
+            #     print('please type only number')
 
         except ValueError:
-             print('please type next operators : + - / *')
+            print(error_number_only)
+
+
+        while True:
+
+                oper = input('Please type operation : + - / *')
+
+                if oper == '+':
+                    result = number_1 + number_2
+
+                elif oper == '-':
+                    result = number_1 - number_2
+
+                elif oper == '*':
+                    result = number_1 * number_2
+
+
+
+                elif oper == '/':
+                    if number_2 == 0:
+                        print("You can't divide by zero")
+                        zero_error = True
+                        break
+                    result =  number_1 / number_2
+
+                else:
+                    print('please type next operators : + - / *')
+                    continue
+                print(result)
+                break
+        if zero_error:
+            continue
+
+        break
+
+    again = input('Try again ? n = no. f = yes')
+    if  again == 'n':
+        break
 
 
 
